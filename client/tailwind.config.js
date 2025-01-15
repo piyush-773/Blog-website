4cfa3512dda03770ee7e6ba5b990512ef97aaffb
piyush-773 <piyushlikeyou@gmail.com>
2025-01-15 23:36:48 +0530

    .

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,23 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     './index.html',
+//     './src/**/*.{js,ts,jsx,tsx}',
+//     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('flowbite/plugin'),
+//     require('tailwind-scrollbar'),
+//   ],
+// };
+
 /** @type {import('tailwindcss').Config} */
+import flowbite from 'flowbite/plugin';
+import scrollbar from 'tailwind-scrollbar';
+
 export default {
   content: [
     './index.html',
@@ -8,8 +27,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin'),
-    require('tailwind-scrollbar'),
-  ],
+  plugins: [flowbite, scrollbar],
 };
