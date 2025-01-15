c45e866df4a411d29eb47abe9ff5f8f84b01f28d
piyush-773 <piyushlikeyou@gmail.com>
2025-01-16 00:38:14 +0530

    .

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -103,9 +103,11 @@ export default function CreatePost() {
             }
           >
             <option value='uncategorized'>Select a category</option>
-            <option value='javascript'>JavaScript</option>
-            <option value='reactjs'>React.js</option>
-            <option value='nextjs'>Next.js</option>
+            <option value='motivation'>Motivation</option>
+            <option value='facts'>Facts</option>
+            <option value='knowledge'>Knowledge</option>
+            <option value='Artical'>Artical</option>
+            <option value='news'>News</option>
           </Select>
         </div>
         <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
