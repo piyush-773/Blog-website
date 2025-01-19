19b96301e7554c338386eb8ef273b34c6b870d3e
Piyush kumar <145182985+piyush-773@users.noreply.github.com>
2025-01-19 10:25:14 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: 'https://factick.vercel.app/',
+  origin: process.env.PROJECT_BASE_URL,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
